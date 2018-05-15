df6aadc63b3b499873b1352b5e97672a24243586
LAPTOP-0LOM1CM3\mimi <ddcc2100@gmail.com>
2018-05-15 20:06:35 +0800

    version1

---
+++ HW04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 將號碼加入List中
+
+            List<int> members = new List<int>();
+
+            for (int i = 1; i <= 42; i++)
+            {
+                members.Add(i);
+            }
+
+            // 隨機一印出
+            Random rand = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                int r = rand.Next(1, members.Count);
+                Console.Write(members[r] + " ");
+
+                // 從List中刪除
+                members.RemoveAt(r);
+            }
+
+            // 停住畫面
+            Console.ReadLine();
+        }
+    }
+}
